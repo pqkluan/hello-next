7063884fd236dbc4a85cf911c23a5bd1c0a167ad
pqkluan <luan@eatsyapp.co>
2020-04-06 11:50:17 +0700

    feat: navigate between pages

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
-export default function Index() {
+import Link from "next/link";
+
+function Home() {
   return (
-    <div>
-      <p>Hello Next.js</p>
-    </div>
+    <ul>
+      <li>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/about">
+          <a>About Us</a>
+        </Link>
+      </li>
+    </ul>
   );
 }
+
+export default Home;
