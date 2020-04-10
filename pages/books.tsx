4de227ccf8571ea7c00eeb2322e34a441717e2fd
pqkluan <luan@eatsyapp.co>
2020-04-10 11:25:53 +0700

    fix: use dynamic graphql endpoint

--- pages/books.tsx
+++ pages/books.tsx
@@ -5,9 +5,14 @@ import fetch from "isomorphic-unfetch";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
 
+const uri =
+  process.env.NODE_ENV === "production"
+    ? "https://whispering-coast-31341.herokuapp.com/graphql"
+    : "http://localhost:4000/graphql";
+
 // TODO: move to graphql dir
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri,
   fetch,
 });
 
