64eed1a5953a0272c109d333c36f3b072d7a4e4e
pqkluan <luan@eatsyapp.co>
2020-04-14 09:22:18 +0700

    feat: update graphql prod url

--- graphql/client.ts
+++ graphql/client.ts
@@ -4,7 +4,7 @@ import ApolloClient from "apollo-boost";
 
 const uri =
   process.env.NODE_ENV === "production"
-    ? "https://whispering-coast-31341.herokuapp.com/graphql"
+    ? "https://whispering-coast-31341.herokuapp.com/"
     : "http://localhost:4000/";
 
 export default new ApolloClient({
